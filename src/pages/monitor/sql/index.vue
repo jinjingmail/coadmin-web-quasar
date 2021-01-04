20fc8a17776fa46e37d83b7361cf3fdbcc4d7de7
jinjin <jinjin@gmail.com>
2021-01-04 12:00:08 +0800

    添加sql监控页面

---
+++ src/pages/monitor/sql/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <my-iframe :src="sqlApi" />
+</template>
+<script>
+import { mapState } from 'vuex'
+import MyIframe from 'components/Iframe'
+export default {
+  name: 'Sql',
+  components: { MyIframe },
+  computed: {
+    ...mapState('api', [
+      'sqlApi'
+    ])
+  }
+}
+</script>
